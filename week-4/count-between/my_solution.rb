6565ea83e720c888963bf352f8ddb0e80bcb71ed
scottychou <iscott.chou@gmail.com>
2015-10-24 15:10:18 -0500

    Add 4.6

---
+++ week-4/count-between/my_solution.rb
@@ -0,0 +1,30 @@
+# Count Between
+
+# I worked on this challenge [by myself, with: ].
+
+# count_between is a method with three arguments:
+#   1. An array of integers
+#   2. An integer lower bound
+#   3. An integer upper bound
+#
+# It returns the number of integers in the array between the lower and upper bounds,
+# including (potentially) those bounds.
+#
+# If +array+ is empty the method should return 0
+
+# Your Solution Below
+
+def count_between(list_of_integers, lower_bound, upper_bound)
+  if list_of_integers.empty? == true
+  	return 0
+  else
+  	count = 0
+  	list_of_integers.each do |x| 
+  		if (x >= lower_bound && x <= upper_bound)
+  			count = count + 1
+  		end
+  	end
+  	p count
+  end
+end
+
---
+++ week-4/shortest-string/my_solution.rb
@@ -0,0 +1,26 @@
+# Shortest String
+
+# I worked on this challenge [by myself, with: ].
+
+# shortest_string is a method that takes an array of strings as its input
+# and returns the shortest string
+#
+# +list_of_words+ is an array of strings
+# shortest_string(array) should return the shortest string in the +list_of_words+
+#
+# If +list_of_words+ is empty the method should return nil
+
+#Your Solution Below
+def shortest_string(list_of_words)
+  if list_of_words.length == 0
+  	p nil 
+  else
+  	shortest = list_of_words[0]
+  	list_of_words.each do |x|
+  		if(x.length < shortest.length)
+  			shortest = x
+  		end
+  	end
+  	p shortest
+  end  
+end
