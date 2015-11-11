f0c3ced1ca6edf772b99258981c5d825de295f47
scottychou <iscott.chou@gmail.com>
2015-11-11 17:43:15 -0600

    Add 7.2

--- week-7/eloquent.js
+++ week-7/eloquent.js
@@ -4,19 +4,33 @@
 
 // Program Structure
 // Write your own variable and do something to it.
-
+var wuddup = 1;
+wuddup = wuddup +1;
 
 
 
 // Complete one of the exercises: Looping a Triangle, FizzBuzz, or Chess Board
-
+var tri = "#";
+i = 0;
+while (i<8){
+	console.log(tri);
+	tri = tri + "#";
+	i++;
+}
 
 
 // Functions
 
 // Complete the `minimum` exercise.
-
+function min(x,y) {
+  if (x < y)
+  	return x;
+  else
+  	return y;
+}
 
 
 // Data Structures: Objects and Arrays
 // Create an object called "me" that stores your name, age, 3 favorite foods, and a quirk below.
+
+var me = ["scott", 27, "pizza", "sushi", "donuts", "really really fucking cool"];
