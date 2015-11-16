a32702ffbfd527c46b2c80604591ad2e66e75755
scottychou <iscott.chou@gmail.com>
2015-11-15 22:24:53 -0600

    Add game

--- week-7/game.js
+++ week-7/game.js
@@ -20,73 +20,129 @@
 //if you lose, display a lose message with how many levels you beat.
 
 // Initial Code
-var counter = 0;
-var values = [1,2,3,4,5,6,7,8,9,10];
-function randomcard() {
-	var card = values[Math.floor(Math.random() * values.length)];
-	return card;
-}
-var dealer = {
-	card : randomcard()
-};
-var player = {
-	card : randomcard()
-};
+// var counter = 0;
+// var values = [1,2,3,4,5,6,7,8,9,10];
+// function randomcard() {
+// 	var card = values[Math.floor(Math.random() * values.length)];
+// 	return card;
+// }
+// var dealer = {
+// 	card : randomcard()
+// };
+// var player = {
+// 	card : randomcard()
+// };
 
 
 
-function guess() {
-	console.log("Dealer has: " + dealer.card);
-	console.log("Please type in higher or lower");
-	var x = prompt("Enter higher or lower","higher");
-	if (counter === 3){
-	    console.log("YOU WON THE FUCKING GAME");
-	}
-	else if (x === "higher"  && player.card > dealer.card){
-		console.log("You guessed right, hidden card was a " + player.card + " and dealer has a " + dealer.card);
-		dealer.card = player.card;
-		player.card = randomcard();
-		counter++;
-		console.log("You are currently at " + counter + "win(s), you need 3 wins to beat the game");
+// function guess() {
+// 	console.log("Dealer has: " + dealer.card);
+// 	console.log("Please type in higher or lower");
+// 	var x = prompt("Enter higher or lower","higher");
+// 	if (counter === 3){
+// 	    console.log("YOU WON THE FUCKING GAME");
+// 	}
+// 	else if (x === "higher"  && player.card > dealer.card){
+// 		console.log("You guessed right, hidden card was a " + player.card + " and dealer has a " + dealer.card);
+// 		dealer.card = player.card;
+// 		player.card = randomcard();
+// 		counter++;
+// 		console.log("You are currently at " + counter + "win(s), you need 3 wins to beat the game");
 	    
-	}
-	else if (x === "lower" && player.card < dealer.card){
-		console.log("You guessed right, hidden card was a " + player.card + " and dealer has a " + dealer.card);
-		dealer.card = player.card;
-		player.card = randomcard();
-		counter++;
-		console.log("You are currently at " + counter + "win(s), you need 3 wins to beat the game");
-	}
-	else {
-		console.log("You guessed wrong, hidden card was a " + player.card + " and dealer has a " + dealer.card);
-		dealer.card = player.card;
-		player.card = randomcard();
-		counter = 0;
-		console.log("Sucks to be you, you are back at a 0 win streak, lmao");
-}
-}
-function gamestart(){
-	console.log("Win 3 in a row, and beat this game.  Let's start:")
-	while (counter < 3){
-	guess();
-	}
-}
+// 	}
+// 	else if (x === "lower" && player.card < dealer.card){
+// 		console.log("You guessed right, hidden card was a " + player.card + " and dealer has a " + dealer.card);
+// 		dealer.card = player.card;
+// 		player.card = randomcard();
+// 		counter++;
+// 		console.log("You are currently at " + counter + "win(s), you need 3 wins to beat the game");
+// 	}
+// 	else {
+// 		console.log("You guessed wrong, hidden card was a " + player.card + " and dealer has a " + dealer.card);
+// 		dealer.card = player.card;
+// 		player.card = randomcard();
+// 		counter = 0;
+// 		console.log("Sucks to be you, you are back at a 0 win streak, lmao");
+// }
+// }
+// function gamestart(){
+// 	console.log("Win 3 in a row, and beat this game.  Let's start:")
+// 	while (counter < 3){
+// 	guess();
+// 	}
+// }
 
-gamestart();
+// gamestart();
 
 // Refactored Code
 
+function randomcard() {
+  var values = [1,2,3,4,5,6,7,8,9,10];
+  var card = values[Math.floor(Math.random() * values.length-1)];
+  return card;
+}
+var dealer = {
+  card: 0
+};
+var player = {
+  card: 0
+};
 
+function guess(counter) {
+  dealer.card = randomcard();
+  player.card = randomcard();
+  console.log("Player Card " + player.card);
+  console.log("Dealer has: " + dealer.card);
+  console.log("Please type in higher or lower");
+  var x = readline("Enter higher or lower");
+  if (counter == 3){
+      console.log("YOU WON THE FUCKING GAME");
+  }
+  else if ((x == "higher")  && (player.card > dealer.card)){
+    console.log("You guessed right, hidden card was a " + player.card + " and dealer has a " + dealer.card);
+    counter++;
+    console.log("counter " + counter);
+    console.log("You are currently at " + counter + "win(s), you need 3 wins to beat the game");
+     
+  }
+  else if ((x == "lower") && (player.card < dealer.card)){
+    console.log("You guessed right, hidden card was a " + player.card + " and dealer has a " + dealer.card);
+    counter++;
+    console.log("counter " + counter);
+    console.log("You are currently at " + counter + "win(s), you need 3 wins to beat the game");
+  }
+  else {
+    console.log("You guessed wrong, hidden card was a " + player.card + " and dealer has a " + dealer.card);
+    counter = 0;
+    console.log("counter " + counter);
+    console.log("Sucks to be you, you are back at a 0 win streak, lmao");
+}
+}
+function gamestart(){
+  var counter = 0;
+  console.log("Win 3 in a row, and beat this game.  Let's start:")
+  while (counter < 3){
+    guess(counter);
+  }
+}
+
+gamestart();
 
 
 
 
 // Reflection
-//
-//
-//
-//
-//
-//
-//
-//
+
+    // What was the most difficult part of this challenge?
+    // -The most difficult part was definitely putting everything I've learned so far together.  I'm only one week into JavaScript so I had to
+    // solidify my knowledge in a short amount of time.
+
+    // What did you learn about creating objects and functions that interact with one another?
+    // -I learned that, to make objects interact with one another, you can just set them equal to something else, or add on a function to the end of an object.
+
+    // Did you learn about any new built-in methods you could use in your refactored solution? If so, what were they and how do they work?
+    // -I didn't learn any new built-in methods this time, but I did get practice on cleaning up my code and making it more readable.
+
+    // How can you access and manipulate properties of objects?
+    // -You can access them by calling the object, then use a period right aftewards to choose with property.
+    // -For example, object.property would call the property, and then you can simply set it equal to whatever you want to change it to.
